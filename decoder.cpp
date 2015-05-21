973a753dfff96cf9881e5dfd46e5b9e2652e6155
new_commander <new_commander@163.com>
2015-05-21 22:51:57 +0800

    2015-05-21 22:51:57 decoder done, analysis next

--- decoder.cpp
+++ decoder.cpp
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#include <time.h>
 
 extern "C" {
 #define __STDC_CONSTANT_MACROS
@@ -22,150 +23,303 @@ extern "C" {
 #include "mylog.h"
 
 #include <vector>
+#include <deque>
 
-struct ch_info {
-    int pid;
-    std::string stream;
+#define DEQUE_MAX_SIZE 10000
+
+#define msleep(x) \
+    do { \
+        struct timespec time_to_wait; \
+        time_to_wait.tv_sec = 0; \
+        time_to_wait.tv_nsec = 1000 * 1000 * x; \
+        nanosleep(&time_to_wait, NULL); \
+    } while(0)
+
+struct deque_info {
+    std::deque<AVPacket> pkt_deque;
+    enum AVMediaType media_type;
+    pthread_t receiving_thread;
+    pthread_t decoding_thread;
+    pthread_mutex_t mutex;
+    int stream_index;
 };
 
-static std::vector<struct ch_info> _s_ch_proc;
-static AVFormatContext *_s_fmt_ctx = NULL;
+struct child_info {
+    std::string file;
+    int pid;
+};
 
-void *decoding(void *type)
+static std::vector<struct child_info> _s_child_processes;  // for global process
+static AVFormatContext *_s_fmt_ctx = NULL;  // for each child process
+
+int insert_pkt(std::deque<AVPacket> &queue, AVPacket *pkt, pthread_mutex_t *mutex)
+{
+    if (!pkt || !mutex) {
+        return 0;
+    }
+
+    pthread_mutex_lock(mutex);
+    if (queue.size() >= DEQUE_MAX_SIZE) {
+        LOG_ERROR("[insert_pkt] insert failed, deque full");
+        pthread_mutex_unlock(mutex);
+        return -1;
+    }
+    queue.push_back(*pkt);
+    pthread_mutex_unlock(mutex);
+
+    return 0;
+}
+
+void pick_pkt(std::deque<AVPacket> &queue, AVPacket *pkt, pthread_mutex_t *mutex)
+{
+    if (!mutex) {
+        pkt->size = 0;
+        return;
+    }
+
+    pthread_mutex_lock(mutex);
+    if (queue.empty()) {
+        pkt->size = 0;
+        pthread_mutex_unlock(mutex);
+        return;
+    }
+    *pkt = queue.front();
+    queue.pop_front();
+    pthread_mutex_unlock(mutex);
+}
+
+void decoding_thread_clean(void *arg)
+{
+    struct deque_info *info = (struct deque_info*)arg;
+    pthread_cancel(info->receiving_thread);
+    LOG_INFO("[decoding_thread_clean] decoding thread exit, for %s stream in file %s",
+            av_get_media_type_string(info->media_type), _s_fmt_ctx->filename);
+}
+
+void receiving_thread_clean(void *arg)
+{
+    struct deque_info *info = (struct deque_info*)arg;
+    pthread_cancel(info->decoding_thread);
+    LOG_INFO("[receiving_thread_clean] receiving thread exit, for %s stream in file %s",
+            av_get_media_type_string(info->media_type), _s_fmt_ctx->filename);
+}
+
+void *decoding_packet(void *arg)
 {
     int ret = 0;
 
-    if (!type) {
+    pthread_cleanup_push(decoding_thread_clean, arg);
+
+    if (!arg) {
         return NULL;
     }
 
-    enum AVMediaType media_type = *(enum AVMediaType*)type;
+    struct deque_info *info = (struct deque_info*)arg;
 
-    ret = av_find_best_stream(_s_fmt_ctx, media_type, -1, -1, NULL, 0);
-    if (ret < 0) {
-        LOG_WARN("[decoding] Error finding stream with type of %s in file %s: %s",
-                av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
-        return NULL;
-    } else if (ret == AVERROR_STREAM_NOT_FOUND) {
-        LOG_WARN("[decoding] not found stream with type of %s in file %s",
+    enum AVMediaType media_type = info->media_type;
+
+    /*
+    int bits_per_pixel = 0;
+    if (media_type == AVMEDIA_TYPE_VIDEO) {
+        AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(_s_fmt_ctx->pix_fmt);
+        if (!desc) {
+            LOG_ERROR("[decoding_packet] error getting pixfmt descriptor, for %s stream in file %s : %s",
+                    av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
+            return NULL;
+        }
+        bits_per_pixel = av_get_bits_per_pixel(desc);
+    } else if (media_type == AVMEDIA_TYPE_AUDIO) {
+        ;//TODO
+    } else {
+        LOG_ERROR("[decoding_packet] unsupport media type %s in file %s",
                 av_get_media_type_string(media_type), _s_fmt_ctx->filename);
         return NULL;
     }
-
-    LOG_DEBUG("[decoding] thread for #%d stream of %s", ret, _s_fmt_ctx->filename);
+    */
 
     AVStream *stream = NULL;
-    stream = _s_fmt_ctx->streams[ret];
+    stream = _s_fmt_ctx->streams[info->stream_index];
 
     AVCodec *dec = NULL;
     dec = avcodec_find_decoder(stream->codec->codec_id);
     if (!dec) {
-        LOG_ERROR("[decoding] Error finding decoder: %s, with type of %s, for file %s",
+        LOG_ERROR("[decoding_packet] Error finding decoder: %s, for %s stream in file %s : %s",
                 avcodec_get_name(stream->codec->codec_id),
-                av_get_media_type_string(media_type), _s_fmt_ctx->filename);
+                av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
         return NULL;
     }
 
     AVDictionary *opt = NULL;
     ret = avcodec_open2(stream->codec, dec, &opt);
     if (ret < 0) {
-        LOG_ERROR("[decoding] open codec %s for file %s failed: %s",
-                avcodec_get_name(stream->codec->codec_id), _s_fmt_ctx->filename, av_err2str(ret));
+        LOG_ERROR("[decoding_packet] open codec: %s failed, for %s stream in file %s : %s",
+                avcodec_get_name(stream->codec->codec_id), av_get_media_type_string(media_type),
+                _s_fmt_ctx->filename, av_err2str(ret));
         return NULL;
     }
-
-    AVPacket pkt;
-    av_init_packet(&pkt);
-    pkt.data = NULL;
-    pkt.size = 0;
 
     AVFrame *frame = NULL;
     frame = av_frame_alloc();
     if (!frame) {
-        LOG_ERROR("[decoding] alloc frame failed for file %s", _s_fmt_ctx->filename);
+        LOG_ERROR("[decoding_packet] alloc frame failed for %s stream in file %s : %s",
+                av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
         avcodec_close(stream->codec);
         return NULL;
     }
 
     int got_frame = 0;
 
+    std::deque<AVPacket> &p_deque = info->pkt_deque;
+    pthread_mutex_t *mutex = &(info->mutex);
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
     while (1) {
-        ret = av_read_frame(_s_fmt_ctx, &pkt);
-        if (ret < 0) {
-            LOG_WARN("[decoding] read frame failed, in file %s : %s",
-                    _s_fmt_ctx->filename, av_err2str(ret));
+        pick_pkt(p_deque, &pkt, mutex);
+        if (pkt.size == 0) {
+            msleep(100);
             continue;
         }
-
-        LOG_DEBUG("[decoding] read frame success, in file %s", _s_fmt_ctx->filename);
-
         do {
             if (media_type == AVMEDIA_TYPE_VIDEO) {
                 ret = avcodec_decode_video2(stream->codec, frame, &got_frame, &pkt);
             } else if (media_type == AVMEDIA_TYPE_AUDIO) {
                 ret = avcodec_decode_audio4(stream->codec, frame, &got_frame, &pkt);
             }
+
             if (ret < 0) {
-                LOG_ERROR("[decoding] Error decoding with type of %s in file %s : %s, \
-                        end up decoding this stream", av_get_media_type_string(media_type),
+                LOG_ERROR("[decoding_packet] Error decoding %s stream in file %s : %s, "
+                        "end up decoding this stream", av_get_media_type_string(media_type),
                         _s_fmt_ctx->filename, av_err2str(ret));
                 goto out;
             }
+
             //size_t unpadded_linesize;
             //unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample((enum AVSampleFormat)frame->format);
+            /*
+            int bytes_per_sample = 0;
+            if (media_type == AVMEDIA_TYPE_VIDEO) {
+                ;//TODO
+            } else if (media_type == AVMEDIA_TYPE_AUDIO) {
+                if (bytes_per_sample == 0) {
+                    bytes_per_sample = av_get_bytes_per_sample((enum AVSampleFormat)frame->format);
+                    if (bytes_per_sample == 0) {
+                        LOG_ERROR("[decoding_packet] error getting bytes per sample for %s stream in file %s : %s",
+                                av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
+                        goto out;
+                    }
+                }
+            }
+            */
+
             pkt.data += ret;
             pkt.size -= ret;
         } while (pkt.size > 0);
     }
 
-    // flushing cache
-    pkt.data = NULL;
-    pkt.size = 0;
-    do {
-        ret = avcodec_decode_audio4(stream->codec, frame, &got_frame, &pkt);
-        if (ret < 0) {
-            LOG_ERROR("[decoding] Error decoding(flushing cache) with type of %s in file %s : %s, \
-                    end up decoding this stream", av_get_media_type_string(media_type),
-                    _s_fmt_ctx->filename, av_err2str(ret));
-            goto out;
-        }
-    } while (got_frame);
-
 out:
     av_frame_free(&frame);
     avcodec_close(stream->codec);
+    pthread_cleanup_pop(1);
     return NULL;
 }
 
-int new_stream(std::string url)
+void *receiving_packet(void *arg)
 {
-    enum AVMediaType media_video = AVMEDIA_TYPE_VIDEO;
-    enum AVMediaType media_audio = AVMEDIA_TYPE_AUDIO;
+    int ret = 0;
+
+    if (!arg) {
+        return NULL;
+    }
+
+    struct deque_info *info = (struct deque_info*)arg;
+
+    enum AVMediaType media_type = info->media_type;
+
+    ret = av_find_best_stream(_s_fmt_ctx, media_type, -1, -1, NULL, 0);
+    if (ret == AVERROR_STREAM_NOT_FOUND) {
+        LOG_WARN("[receiving_packet] not found %s stream in file %s",
+                av_get_media_type_string(media_type), _s_fmt_ctx->filename);
+        return NULL;
+    } else if (ret < 0) {
+        LOG_ERROR("[receiving_packet] Error finding %s stream in file %s : %s",
+                av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
+        return NULL;
+    }
+
+    info->stream_index = ret;
+
+    LOG_INFO("[receiving_packet] start thread for %s stream in file %s",
+            av_get_media_type_string(media_type), _s_fmt_ctx->filename);
+
+    pthread_t decoding_thread;
+    if ((ret = pthread_create(&decoding_thread, NULL, decoding_packet, info)) != 0) {
+        LOG_WARN("[receiving_packet] failed to create decoding packet thread, "
+                "for %s stream in file %s : %s", av_get_media_type_string(media_type),
+                _s_fmt_ctx->filename, strerror(ret));
+        LOG_WARN("[receiving_packet] receiving thread exit, for %s stream in file %s",
+                av_get_media_type_string(media_type), _s_fmt_ctx->filename);
+        return NULL;
+    }
+
+    pthread_cleanup_push(receiving_thread_clean, info);
+
+    std::deque<AVPacket> &p_deque = info->pkt_deque;
+    pthread_mutex_t *mutex = &(info->mutex);
+
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
+    while (1) { // TODO : how to break ?
+        ret = av_read_frame(_s_fmt_ctx, &pkt);
+        if (ret < 0) {
+            LOG_WARN("[receiving_packet] read frame failed, for %s stream in file %s : %s",
+                    av_get_media_type_string(media_type), _s_fmt_ctx->filename, av_err2str(ret));
+            continue;
+        }
+
+        LOG_DEBUG("[receiving_packet] read frame success, in file %s", _s_fmt_ctx->filename);
+
+        insert_pkt(p_deque, &pkt, mutex);
+    }
+
+    pthread_cleanup_pop(1);
+    return NULL;
+}
+
+int new_file(std::string url)
+{
     pthread_t video_thread;
     pthread_t audio_thread;
     int ret = 0;
 
     ret = fork();
     if (ret < 0) {
-        LOG_ERROR("[new_stream] fork failed: %s", strerror(errno));
+        LOG_ERROR("[new_file] fork failed for file %s : %s", url.c_str(), strerror(errno));
         return ret;
     } else if (ret == 0){
         // child do not live alone
         ret = prctl(PR_SET_PDEATHSIG, SIGKILL);
         if (ret < 0) {
-            LOG_WARN("[new_stream] prctl failed: %s", strerror(errno));
+            LOG_WARN("[new_file] prctl failed for file %s : %s", url.c_str(), strerror(errno));
         }
         if (getppid() == 1) {
             // parent already exit
-            LOG_ERROR("[new_stream] parent process alread exit, \
-                    this process which deal with stream: '%s' will also exit!", url.c_str());
+            LOG_ERROR("[new_file] parent process alread exit, "
+                    "child exit now! for file %s", url.c_str());
             exit(1);
         }
     } else {
         return ret;
     }
 
-    LOG_DEBUG("[new_stream] launch stream: %s", url.c_str());
+    _s_child_processes.clear();
+
+    LOG_INFO("[new_file] launch file %s", url.c_str());
 
     av_register_all();
     avcodec_register_all();
@@ -174,34 +328,48 @@ int new_stream(std::string url)
 
     ret = avformat_open_input(&_s_fmt_ctx, url.c_str(), NULL, NULL);
     if (ret < 0) {
-        LOG_ERROR("[new_stream] open input failed: %s", av_err2str(ret));
+        LOG_ERROR("[new_file] open input failed for file %s : %s",
+                url.c_str(), av_err2str(ret));
         avformat_network_deinit();
         return -1;
     }
 
     ret = avformat_find_stream_info(_s_fmt_ctx, NULL);
     if (ret < 0) {
-        LOG_ERROR("[new_stream] find stream info failed: %s", av_err2str(ret));
+        LOG_ERROR("[new_file] find stream info failed for file %s : %s",
+                url.c_str(), av_err2str(ret));
         avformat_close_input(&_s_fmt_ctx);
         avformat_network_deinit();
         return -1;
     }
 
-    if (pthread_create(&video_thread, NULL, decoding, &media_video) != 0) {
-        LOG_WARN("[new_stream] create video handling thread failed");
+    struct deque_info video_info;
+    video_info.media_type = AVMEDIA_TYPE_VIDEO;
+    pthread_mutex_init(&video_info.mutex, NULL);
+    video_info.pkt_deque.clear();
+    if ((ret = pthread_create(&video_thread, NULL, receiving_packet, &video_info)) != 0) {
+        LOG_WARN("[new_file] failed to create receiving packet thread, for video stream in file %s : %s",
+                url.c_str(), strerror(ret));
     }
+    video_info.receiving_thread = video_thread;
 
-    if (pthread_create(&audio_thread, NULL, decoding, &media_audio) != 0) {
-        LOG_WARN("[new_stream] create audio handling thread failed");
+    struct deque_info audio_info;
+    audio_info.media_type = AVMEDIA_TYPE_AUDIO;
+    pthread_mutex_init(&audio_info.mutex, NULL);
+    audio_info.pkt_deque.clear();
+    if ((ret = pthread_create(&audio_thread, NULL, receiving_packet, &audio_info)) != 0) {
+        LOG_WARN("[new_file] failed to create receiving packet thread, for audio stream in file %s : %s",
+                url.c_str(), strerror(ret));
     }
+    audio_info.receiving_thread = audio_thread;
 
-    LOG_DEBUG("[new_stream] start play the stream: %s", url.c_str());
+    LOG_INFO("[new_file] playing the file %s", url.c_str());
     av_read_play(_s_fmt_ctx);
 
     pthread_join(video_thread, NULL);
     pthread_join(audio_thread, NULL);
 
-    LOG_DEBUG("[new_stream] close the stream: %s", url.c_str());
+    LOG_INFO("[new_file] close the file %s", url.c_str());
     avformat_close_input(&_s_fmt_ctx);
     avformat_network_deinit();
 
@@ -250,10 +418,10 @@ int prepar_socket(int port, int backlog)
     return fd;
 }
 
-int start(int port, std::vector<std::string> *streams)
+int start(int port, std::vector<std::string> *files)
 {
-    std::vector<struct ch_info>::iterator child_iter;
-    std::vector<std::string>::iterator stream_iter;
+    std::vector<struct child_info>::iterator child_iter;
+    std::vector<std::string>::iterator file_iter;
     struct event_base *base = NULL;
     struct evhttp *httpd = NULL;
     int ret = 0;
@@ -293,21 +461,23 @@ int start(int port, std::vector<std::string> *streams)
     evhttp_set_cb(httpd, "/stream_control", stream_control, NULL);
     //evhttp_set_gencb(httpd, gen_handler, NULL);
 
-    _s_ch_proc.clear();
-    for (stream_iter = streams->begin(); stream_iter != streams->end(); stream_iter++) {
-        int pid = new_stream(*stream_iter);
+    _s_child_processes.clear();
+    for (file_iter = files->begin(); file_iter != files->end(); file_iter++) {
+        int pid = new_file(*file_iter);
         if (pid > 0) {
-            struct ch_info tt;
+            struct child_info tt;
             tt.pid = pid;
-            tt.stream = *stream_iter;
-            _s_ch_proc.push_back(tt);
+            tt.file = *file_iter;
+            _s_child_processes.push_back(tt);
+        } else {
+            break;
         }
     }
 
     event_base_dispatch(base);
 
 out:
-    for (child_iter = _s_ch_proc.begin(); child_iter != _s_ch_proc.end(); child_iter++) {
+    for (child_iter = _s_child_processes.begin(); child_iter != _s_child_processes.end(); child_iter++) {
         kill(child_iter->pid, SIGKILL);
     }
 
@@ -416,7 +586,7 @@ int main(int argc, char **argv)
         return 1;
     } else {
         if (!config["log_file"].isString()) {
-            fprintf(stderr, "config item 'log_file' is not a string\n");
+            fprintf(stderr, "[main] config item 'log_file' is not a string\n");
             return 1;
         }
     }
@@ -425,7 +595,7 @@ int main(int argc, char **argv)
         return 1;
     } else {
         if (!config["log_leave"].isInt()) {
-            fprintf(stderr, "config item 'log_leave' is not a integer\n");
+            fprintf(stderr, "[main] config item 'log_leave' is not a integer\n");
             return 1;
         }
     }
